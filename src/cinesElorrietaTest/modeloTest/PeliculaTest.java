13d28a1ec8857dfd33ad91bc94a52ff25d7dbdb3
in1dm3-v <in1dm3-v@HZ317744.elorrieta.int>
2024-01-26 17:45:54 +0100

    creación del test de la clase "Pelicula"

---
+++ src/cinesElorrietaTest/modeloTest/PeliculaTest.java
@@ -0,0 +1,14 @@
+package cinesElorrietaTest.modeloTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PeliculaTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
