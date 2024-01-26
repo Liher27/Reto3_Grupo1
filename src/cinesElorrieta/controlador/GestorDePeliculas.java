09d3bd7028f61a6b866aca3e3b923c7f41fdf676
in1dm3-v <in1dm3-v@HZ327441.elorrieta.int>
2024-01-26 16:51:19 +0100

    Modificacion de la clase de "GestorDePeliculas"

--- src/cinesElorrieta/controlador/GestorDePeliculas.java
+++ src/cinesElorrieta/controlador/GestorDePeliculas.java
@@ -1,10 +1,21 @@
 package cinesElorrieta.controlador;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import cinesElorrieta.modelo.Pelicula;
+
+/**
+ * Clase que gestiona los alumno
+ */
 public class GestorDePeliculas {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private static Scanner teclado = null;
+	private List<Pelicula> peliculas = null;
 
+	public GestorDePeliculas() {
+		teclado = new Scanner(System.in);
+		peliculas = new ArrayList<Pelicula>();
 	}
-
 }
