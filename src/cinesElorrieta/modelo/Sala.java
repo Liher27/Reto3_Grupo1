de5b24f672017b29e8a7ed7cebe4d11b2971d375
Liher27 <liher.chamorropa@elorrieta-errekamari.com>
2024-02-05 18:32:19 +0100

    cambios

--- src/cinesElorrieta/modelo/Sala.java
+++ src/cinesElorrieta/modelo/Sala.java
@@ -5,15 +5,9 @@ import java.io.Serializable;
 import java.util.Objects;
 
 
+
 public class Sala implements Serializable {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -4693711428181363078L;
-	private int CodSala;
-	private String NomSala;
-	private int CodCine;
 	@Override
 	public int hashCode() {
 		return Objects.hash(CodCine, CodSala, NomSala);
@@ -29,6 +23,16 @@ public class Sala implements Serializable {
 		Sala other = (Sala) obj;
 		return CodCine == other.CodCine && CodSala == other.CodSala && Objects.equals(NomSala, other.NomSala);
 	}
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4693711428181363078L;
+	private int CodSala;
+	private String NomSala;
+	private int CodCine;
+	
+	
+	
 	@Override
 	public String toString() {
 		return "Sala [CodSala=" + CodSala + ", NomSala=" + NomSala + ", CodCine=" + CodCine + "]";
