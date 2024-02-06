49a579673264e03ca1ff57924473f512e8697df8
inigoaguirre02 <inigoaguirre2002@gmail.com>
2024-02-06 20:06:56 +0100

    Diseño panel de registro

---
+++ src/cienElorrieta/ventana/DiseñoPaneles.java
@@ -0,0 +1,160 @@
+package cienElorrieta.ventana;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import java.awt.Color;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+
+import java.awt.Font;
+import javax.swing.JComboBox;
+
+public class DiseñoPaneles extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_5;
+	private JTextField textField_6;
+	private JTextField textField_4;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					DiseñoPaneles frame = new DiseñoPaneles();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public DiseñoPaneles() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1000, 650);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(42, 26, 29));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("DNI");
+		lblNewLabel.setForeground(new Color(253, 185, 74));
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 22));
+		lblNewLabel.setBounds(192, 130, 86, 43);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Nombre");
+		lblNewLabel_1.setForeground(new Color(253, 185, 74));
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 22));
+		lblNewLabel_1.setBounds(192, 200, 86, 27);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("Primer apellido");
+		lblNewLabel_2.setForeground(new Color(253, 185, 74));
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 22));
+		lblNewLabel_2.setBounds(192, 270, 176, 32);
+		contentPane.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("Segundo apellido");
+		lblNewLabel_3.setForeground(new Color(253, 185, 74));
+		lblNewLabel_3.setFont(new Font("Tahoma", Font.PLAIN, 22));
+		lblNewLabel_3.setBounds(192, 340, 176, 32);
+		contentPane.add(lblNewLabel_3);
+		
+		JLabel lblNewLabel_4 = new JLabel("Contraseña");
+		lblNewLabel_4.setForeground(new Color(253, 185, 74));
+		lblNewLabel_4.setFont(new Font("Tahoma", Font.PLAIN, 22));
+		lblNewLabel_4.setBounds(192, 410, 159, 31);
+		contentPane.add(lblNewLabel_4);
+		
+		JLabel lblNewLabel_5 = new JLabel("Confirmar contraseña");
+		lblNewLabel_5.setForeground(new Color(253, 185, 74));
+		lblNewLabel_5.setFont(new Font("Tahoma", Font.PLAIN, 22));
+		lblNewLabel_5.setBounds(192, 480, 216, 32);
+		contentPane.add(lblNewLabel_5);
+		
+		JLabel lblNewLabel_6 = new JLabel("Sexo");
+		lblNewLabel_6.setForeground(new Color(253, 185, 74));
+		lblNewLabel_6.setFont(new Font("Tahoma", Font.PLAIN, 22));
+		lblNewLabel_6.setBounds(192, 550, 230, 32);
+		contentPane.add(lblNewLabel_6);
+		
+		textField = new JTextField();
+		textField.setBounds(513, 146, 176, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(513, 208, 176, 20);
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
+		
+		textField_2 = new JTextField();
+		textField_2.setBounds(513, 281, 176, 20);
+		contentPane.add(textField_2);
+		textField_2.setColumns(10);
+		
+		textField_3 = new JTextField();
+		textField_3.setBounds(513, 351, 176, 20);
+		contentPane.add(textField_3);
+		textField_3.setColumns(10);
+		
+		textField_5 = new JTextField();
+		textField_5.setBounds(513, 491, 176, 20);
+		contentPane.add(textField_5);
+		textField_5.setColumns(10);
+		
+		textField_6 = new JTextField();
+		textField_6.setBounds(513, 420, 176, 20);
+		contentPane.add(textField_6);
+		textField_6.setColumns(10);
+		
+		textField_4 = new JTextField();
+		textField_4.setBounds(513, 561, 176, 20);
+		contentPane.add(textField_4);
+		textField_4.setColumns(10);
+		
+		JLabel lblNewLabel_7 = new JLabel("Registrar nuevo usuario");
+		lblNewLabel_7.setForeground(new Color(253, 185, 74));
+		lblNewLabel_7.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 28));
+		lblNewLabel_7.setBounds(328, 32, 337, 64);
+		contentPane.add(lblNewLabel_7);
+		
+		JLabel lblLogoCineElorrieta = new JLabel("");
+		lblLogoCineElorrieta.setHorizontalAlignment(SwingConstants.CENTER);
+		lblLogoCineElorrieta.setBounds(34, 32, 90, 90);
+		lblLogoCineElorrieta.setIcon(new ImageIcon("src/LogoCineElorrieta.png"));
+		contentPane.add(lblLogoCineElorrieta);
+		
+		JButton btnNewButton = new JButton("Cancelar");
+		btnNewButton.setForeground(new Color(0, 0, 0));
+		btnNewButton.setBounds(35, 577, 89, 23);
+		contentPane.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Confirmar");
+		btnNewButton_1.setForeground(new Color(0, 0, 0));
+		btnNewButton_1.setBounds(849, 577, 101, 23);
+		contentPane.add(btnNewButton_1);
+		
+		
+}
+}
