ae97fb09756f47383cb21e6191e6d89fb34e3dcc
Liher27 <liher.chamorropa@elorrieta-errekamari.com>
2024-02-09 17:06:14 +0100

    Revert "Otro cambio"
    This reverts commit 7a15c35c9a7f63d82e23f797951c19b818a00f3b.

--- src/cinesElorrieta/vista/complementos/PanelDeCines.java
+++ src/cinesElorrieta/vista/complementos/PanelDeCines.java
@@ -99,10 +99,7 @@ public class PanelDeCines{
 		paneles.get(0).setVisible(false);
 		paneles.get(1).setVisible(false);
 		paneles.get(2).setVisible(false);
-		paneles.get(3).setVisible(false);
-		paneles.get(4).setVisible(false);
-		paneles.get(5).setVisible(false);
-		paneles.get(6).setVisible(true);
+		paneles.get(3).setVisible(true);
 			}
 	});
 		panelDeCines.setLayout(null);
