1d44d0cd749b8b2d212419cc6a628b89a24ee790
Liher27 <liher.chamorropa@elorrieta-errekamari.com>
2024-02-15 18:53:05 +0100

    Revert al anterior push

---
+++ src/cinesElorrieta/vista/complementos/PanelDePeliculas.java
@@ -0,0 +1,146 @@
+package cinesElorrieta.vista.complementos;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.SwingConstants;
+import javax.swing.table.DefaultTableModel;
+
+import cinesElorrieta.bbdd.Pelicula;
+import cinesElorrieta.bbdd.Sesion;
+import cinesElorrieta.logica.GestorDePeliculas;
+import cinesElorrieta.logica.Session;
+
+public class PanelDePeliculas {
+
+	private JPanel panelDePeliculas;
+	public JTable tablaDePeliculas = null;
+	public static String code; 
+	/**
+	 * Launch the application.
+	 */
+
+	public PanelDePeliculas(ArrayList<JPanel> paneles) {
+		
+		panelDePeliculas = new JPanel();
+		panelDePeliculas.setBounds(0, 0, 984, 611);
+		panelDePeliculas.setBackground(new Color(42, 26, 29));
+		panelDePeliculas.setLayout(null);
+
+		JLabel tituloPanelDeRegistro = new JLabel("Selección de peliculas");
+		tituloPanelDeRegistro.setForeground(new Color(253, 185, 74));
+		tituloPanelDeRegistro.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 28));
+		tituloPanelDeRegistro.setBounds(328, 32, 337, 64);
+		panelDePeliculas.add(tituloPanelDeRegistro);
+		
+	
+		JScrollPane peliculaScroll = new JScrollPane();
+		peliculaScroll.setBounds(235, 127, 512, 195);
+		panelDePeliculas.add(peliculaScroll);
+		
+		 DefaultTableModel modelo = new DefaultTableModel();
+		 tablaDePeliculas = new JTable(modelo);
+		 modelo.addColumn("CodPelicula");
+		 modelo.addColumn("Nombre");
+		 modelo.addColumn("Genero");
+		 modelo.addColumn("Duracion");
+		 modelo.addColumn("Precio");
+		 displayPelis( modelo);
+		 
+		 tablaDePeliculas.setBounds(235, 127, 512, 195);
+		
+		 peliculaScroll.setViewportView(tablaDePeliculas);
+		
+        
+		JButton btnVolverPanelDePeliculas = new JButton("Volver");
+		btnVolverPanelDePeliculas.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				panelDePeliculas.setVisible(false);
+
+				paneles.get(0).setVisible(true);
+				paneles.get(1).setVisible(false);
+				paneles.get(2).setVisible(false);
+			}
+		});
+		btnVolverPanelDePeliculas.setForeground(new Color(0, 0, 0));
+		btnVolverPanelDePeliculas.setBounds(100, 556, 98, 33);
+		panelDePeliculas.add(btnVolverPanelDePeliculas);
+
+		JButton btnContinuarPanelDePeliculas = new JButton("Continuar");
+		btnContinuarPanelDePeliculas.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+
+				/*panelDePeliculas.setVisible(false);
+				 
+				paneles.get(0).setVisible(false);
+				paneles.get(1).setVisible(false);
+				paneles.get(2).setVisible(false);
+				paneles.get(3).setVisible(true);*/
+			
+				
+			}
+		});
+		btnContinuarPanelDePeliculas.setForeground(new Color(0, 0, 0));
+		btnContinuarPanelDePeliculas.setBounds(800, 556, 98, 33);
+		panelDePeliculas.add(btnContinuarPanelDePeliculas);
+
+		JLabel lblLogoCineElorrieta = new JLabel("");
+		lblLogoCineElorrieta.setHorizontalAlignment(SwingConstants.CENTER);
+		lblLogoCineElorrieta.setBounds(33, 33, 90, 90);
+		lblLogoCineElorrieta.setIcon(new ImageIcon("src/LogoCineElorrieta.png"));
+		panelDePeliculas.add(lblLogoCineElorrieta);
+		
+		
+		JButton buscarSesion = new JButton("Buscar Sesion");
+		buscarSesion.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+		
+			int peliculaSeleccionada =tablaDePeliculas.getSelectedRow();
+			int column1 = tablaDePeliculas.getSelectedColumnCount()-1;
+			
+			String code = (String) tablaDePeliculas.getValueAt(peliculaSeleccionada, column1);
+			
+			System.out.println(code);
+	
+	
+			panelDePeliculas.setVisible(false);
+			 
+			paneles.get(0).setVisible(false);
+			paneles.get(1).setVisible(false);
+			paneles.get(2).setVisible(false);
+			paneles.get(3).setVisible(true);
+			}
+			
+		});
+			
+		buscarSesion.setBounds(390, 343, 192, 43);
+		panelDePeliculas.add(buscarSesion);
+		
+	}
+
+	public JPanel inicializarPanelDePeliculas() {
+		return panelDePeliculas;
+	}
+	private void displayPelis(DefaultTableModel modelo) {
+		GestorDePeliculas gestorDePeliculas = new GestorDePeliculas();
+		List<Pelicula> pelis = gestorDePeliculas.getLasPeliculas();
+		for (int i = 0; i < pelis.size(); i++) {
+			String[] linea = { Integer.toString(pelis.get(i).getCodPelicula()), pelis.get(i).getNombre(), pelis.get(i).getGenero(), Integer.toString(pelis.get(i).getDuracion()), Float.toString(pelis.get(i).getPrecio()) };
+			modelo.addRow(linea);
+		}
+	}
+	
+}
---
+++ src/cinesElorrieta/vista/complementos/PanelDeSesion.java
@@ -0,0 +1,139 @@
+package cinesElorrieta.vista.complementos;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.sql.Date;
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.SwingConstants;
+import javax.swing.table.DefaultTableModel;
+
+import cinesElorrieta.bbdd.Sesion;
+import cinesElorrieta.logica.Session;
+import cinesElorrieta.vista.complementos.PanelDePeliculas;
+import javax.swing.JComboBox;
+
+public class PanelDeSesion {
+//*/
+	private JPanel panelDeSesion;
+
+	/**
+	 * Launch the application.
+	 */
+
+	public PanelDeSesion(ArrayList<JPanel> paneles) {
+		
+		panelDeSesion = new JPanel();
+		panelDeSesion.setBounds(0, 0, 984, 611);
+		panelDeSesion.setBackground(new Color(42, 26, 29));
+		panelDeSesion.setLayout(null);
+
+		JLabel tituloPanelDeRegistro = new JLabel("Sesion de la pelicula");
+		tituloPanelDeRegistro.setForeground(new Color(253, 185, 74));
+		tituloPanelDeRegistro.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 28));
+		tituloPanelDeRegistro.setBounds(328, 32, 337, 64);
+		panelDeSesion.add(tituloPanelDeRegistro);
+		
+		  Session gestorDeSesion = new Session();
+		  /*PanelDePeliculas panel = new PanelDePeliculas(paneles);
+		  String key = panel.*/
+		  
+		  List<Sesion> sesiones = gestorDeSesion.seleccionarPeliculaParaSesion(PanelDePeliculas.code);
+		 
+		  String []
+		  index={"CodSesion","CodSala","CodPelicula","Fecha","Hora","Precio"};
+		  Object[][] data = new Object[sesiones.size()][index.length]; for (int i=0; i
+		  < sesiones.size();i++) { Sesion Sesion = sesiones.get(i); data[i][0] =
+		  Sesion.getCodSesion(); data[i][1] = Sesion.getCodSala(); data[i][2] =
+		 Sesion.getCodPelicula(); data[i][3] = Sesion.getFecha(); data[i][4] =
+		  Sesion.getHora(); data[i][5] = Sesion.getPrecioSesion(); }
+		 
+		JScrollPane sesionScroll = new JScrollPane();
+		sesionScroll.setBounds(235, 127, 512, 195);
+		panelDeSesion.add(sesionScroll);
+
+		DefaultTableModel modelo = new DefaultTableModel(data, index);
+		JTable tablaDeSesion = new JTable(modelo);
+		tablaDeSesion.setBounds(235, 127, 512, 195);
+
+		sesionScroll.setViewportView(tablaDeSesion);
+
+		JButton btnVolverPanelDeSesion = new JButton("Volver");
+		btnVolverPanelDeSesion.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				panelDeSesion.setVisible(false);
+
+				paneles.get(0).setVisible(true);
+				paneles.get(1).setVisible(false);
+				paneles.get(2).setVisible(false);
+			}
+		});
+		btnVolverPanelDeSesion.setForeground(new Color(0, 0, 0));
+		btnVolverPanelDeSesion.setBounds(100, 556, 98, 33);
+		panelDeSesion.add(btnVolverPanelDeSesion);
+
+		JButton btnContinuarPanelDeSesion = new JButton("Continuar");
+		btnContinuarPanelDeSesion.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+
+				// wawdaww
+				panelDeSesion.setVisible(false);
+
+				paneles.get(0).setVisible(false);
+				paneles.get(1).setVisible(true);
+			}
+		});
+		btnContinuarPanelDeSesion.setForeground(new Color(0, 0, 0));
+		btnContinuarPanelDeSesion.setBounds(800, 556, 98, 33);
+		panelDeSesion.add(btnContinuarPanelDeSesion);
+
+		JLabel lblLogoCineElorrieta = new JLabel("");
+		lblLogoCineElorrieta.setHorizontalAlignment(SwingConstants.CENTER);
+		lblLogoCineElorrieta.setBounds(33, 33, 90, 90);
+		lblLogoCineElorrieta.setIcon(new ImageIcon("src/LogoCineElorrieta.png"));
+		panelDeSesion.add(lblLogoCineElorrieta);
+
+		PanelDePeliculas panelDePeliculas = new PanelDePeliculas(paneles);
+		panelDePeliculas.tablaDePeliculas.getSelectedRow();
+
+		JComboBox<Sesion> ComboBoxFecha = new JComboBox<Sesion>(new Vector<Sesion>(sesiones));
+		ComboBoxFecha.setBounds(235, 421, 126, 31);
+		panelDeSesion.add(ComboBoxFecha);
+
+		JComboBox<Sesion> ComboBoxHora = new JComboBox<Sesion>(new Vector<Sesion>(sesiones));
+		ComboBoxHora.setBounds(621, 421, 126, 31);
+		panelDeSesion.add(ComboBoxHora);
+	}
+
+	public JPanel inicializarPanelDeSesion() {
+		return panelDeSesion;
+	}
+
+	public void rellenarTabla(String code) {
+			
+	}
+	
+	/*private void displaySesiones(DefaultTableModel modelo) {
+		GestorDeSesion gestorDePeliculas = new GestorDeSesion();
+		List<Sesion> sesiones = gestorDePeliculas.seleccionarPeliculaParaSesion();
+		for (int i = 0; i < sesiones.size(); i++) {
+			String[] linea = { Integer.toString(sesiones.get(i).getCodSesion()), Integer.toString(sesiones.get(i).getCodSala()),
+					 Integer.toString(sesiones.get(i).getCodPelicula()),  Date.toString(sesiones.get(i).getFecha()),Time.toString(sesiones.get(i).getHora()) };
+			modelo.addRow(linea);
+		}
+	}*/
+
+}
